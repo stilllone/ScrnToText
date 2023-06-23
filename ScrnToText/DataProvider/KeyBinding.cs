a0aeed2a264170881d62b908f53a623814049deb
Ev1k <evikezzz4@gmail.com>
2023-06-24 02:51:47 +0300

    KeyBinding template

---
+++ ScrnToText/DataProvider/KeyBinding.cs
@@ -0,0 +1,33 @@
+﻿using ScrnToText.Interface;
+using System.Windows.Input;
+
+namespace ScrnToText.DataProvider
+{
+    public class KeyBinding : IKeyBinding
+    {
+        public KeyBinding() 
+        {
+            LoadKeys();
+        }
+
+        public Key FirstKey { get; set; }
+        public Key? SecondKey { get; set; }
+
+        public (Key, Key?) GetKey()
+        {
+            return (FirstKey, SecondKey);
+        }
+
+        public (Key, Key?) LoadKeys()
+        {
+            // Get data from files
+            return default;
+        }
+
+        public void SetKeys(Key firstKey, Key? secondKey)
+        {
+            FirstKey = firstKey;
+            SecondKey = secondKey;
+        }
+    }
+}
---
+++ ScrnToText/Interface/IKeyBinding.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace ScrnToText.Interface
+{
+    public interface IKeyBinding
+    {
+        public Key FirstKey { get; set; }
+        public Key? SecondKey { get; set; }
+        public void SetKeys(Key firstKey, Key? secondKey);
+        public (Key, Key?) GetKey();
+        public (Key, Key?) LoadKeys();
+    }
+}
